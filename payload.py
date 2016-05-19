d1f85f32d94a9b612aca7d91b4c54752f88fa1ba
Egor Kovetskiy <e.kovetskiy@office.ngs.ru>
2016-05-19 18:52:13 +0600

    fix identifier regexp

--- payload.py
+++ payload.py
@@ -2,7 +2,7 @@ import ycmd
 import ycmd.identifier_utils
 import re
 
-regex = re.compile( r"[_a-zA-Z-][\w:-]*", re.UNICODE)
+regex = re.compile( r"[:_a-zA-Z0-9-]+", re.UNICODE)
 
 ycmd.identifier_utils.FILETYPE_TO_IDENTIFIER_REGEX["sh"]   = regex
 ycmd.identifier_utils.FILETYPE_TO_IDENTIFIER_REGEX["bash"] = regex
--- pythonx/ycmsh.py
+++ pythonx/ycmsh.py
@@ -52,7 +52,7 @@ def hijack_ycmd_identifiers_regex():
     import ycmd
     import ycmd.identifier_utils
 
-    regex = re.compile( r"[_a-zA-Z-][\w:-]*", re.UNICODE)
+    regex = re.compile( r"[:_a-zA-Z0-9-]+", re.UNICODE)
 
     ycmd.identifier_utils.FILETYPE_TO_IDENTIFIER_REGEX["sh"]   = regex
     ycmd.identifier_utils.FILETYPE_TO_IDENTIFIER_REGEX["bash"] = regex
