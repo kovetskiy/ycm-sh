eb91370b874c1b51e386c99c25af1b60358969cd
Stanislav Seletskiy <s.seletskiy@gmail.com>
2017-05-10 15:32:57 +0300

    instantly remove temp file and compiled code

--- payload.py
+++ payload.py
@@ -1,9 +1,13 @@
 import ycmd
 import ycmd.identifier_utils
 import re
+import os
 
-regex = re.compile( r"[:_a-zA-Z0-9-]+", re.UNICODE)
+regex = re.compile(r"[:_a-zA-Z0-9-]+", re.UNICODE)
 
-ycmd.identifier_utils.FILETYPE_TO_IDENTIFIER_REGEX["sh"]   = regex
+ycmd.identifier_utils.FILETYPE_TO_IDENTIFIER_REGEX["sh"] = regex
 ycmd.identifier_utils.FILETYPE_TO_IDENTIFIER_REGEX["bash"] = regex
-ycmd.identifier_utils.FILETYPE_TO_IDENTIFIER_REGEX["zsh"]  = regex
+ycmd.identifier_utils.FILETYPE_TO_IDENTIFIER_REGEX["zsh"] = regex
+
+os.unlink(__file__)
+os.unlink(__file__ + "c")
